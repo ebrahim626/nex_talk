02f089da297bc8baf284578fb26ec634ba6f8e39
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2026-06-26 19:26:53 +0600

    hive and provider api client added

---
+++ lib/src/core/config/constant/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String hiveKey = 'nex-talk-hive';
+  static const String wishListKey = "";
+  static const String stripePublishableKeyTest = '';
+  static const String stripePublishableKeyLive = '';
+}
---
+++ lib/src/core/network/dio/api_endpoints.dart
@@ -0,0 +1,16 @@
+part of '../api_client.export.dart';
+
+class ApiEndpoints {
+
+  ///base url
+  static const String baseUrl = 'https://moderntodoappapi-production.up.railway.app';
+
+  static const String googleSignInEndpoint = '/api/user/google-login';
+  static const String getAllTasksEndpoint = '/api/todo';
+  static const String getProfileEndpoint = '/api/user/profile';
+  static const String getNotificationsEndpoint = '/api/notifications';
+  static const String markAsReadEndpoint = '/api/notifications/mark-as-read';
+  static String editTaskEndPoint({required int slotId}) =>
+      "$getAllTasksEndpoint/$slotId";
+
+}
