1f098f017cd24fd95f572a9c0a154d03b4b9a015
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2026-07-01 21:56:44 +0600

    app route for profile screen name set

--- lib/src/core/router/app_routes.dart
+++ lib/src/core/router/app_routes.dart
@@ -3,5 +3,6 @@ class AppRoutes {
   static const String searchRoute = "/home";
   static const String loginRoute = "/login-screen";
   static const String directChatScreenRoute = "/direct-chat-screen";
+  static const String profileScreenRoute = "/profile-screen";
 
 }
